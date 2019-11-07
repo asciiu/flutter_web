7241eeb6070ff3fb0229a4a34354a89084308c0d
Flowmaster <ellyssin.gimhae@gmail.com>
2019-11-06 20:01:56 -0700

    Added gradient for background.

--- lib/main.dart
+++ lib/main.dart
@@ -57,8 +57,25 @@ class _MyHomePageState extends State<MyHomePage> {
       /// children. It doesn't take up any space itself, so it
       /// can be used as a placeholder in your code.
       body: Container(
-         child: DogList(initialDoggos)
-      ),
+          // Add box decoration
+          decoration: BoxDecoration(
+            // Box decoration takes a gradient
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.5, 0.7, 0.9],
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+                Colors.indigo[800],
+                Colors.indigo[700],
+                Colors.indigo[600],
+                Colors.indigo[400],
+              ],
+            ),
+          ),
+          child: DogList(initialDoggos)),
     );
   }
 }
